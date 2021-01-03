445aa2d4ef4b76505262e9b7c99caed09f68e79d
mljustinli <ml.justin.li@gmail.com>
2021-01-03 18:56:15 -0500

    Attempt nav button animation

--- js/nav.js
+++ js/nav.js
@@ -1,44 +1,66 @@
-var lastActiveSectionElement;
+let navOpen = false;
+let navButton = document.getElementById("nav-button");
+let navButtonBarTop = navButton.children[0];
+let navButtonBarMid = navButton.children[1];
+let navButtonBarBot = navButton.children[2];
 
-// scroll to element with id of destination
-function navTo(destination) {
-    var elmnt = document.getElementById(destination);
-    elmnt.scrollIntoView({ behavior: "smooth" });
+function navButtonClicked() {
+    console.log(navButton);
+    console.log(navButtonBarTop.id);
+    if (navOpen) {
+        navButtonBarTop.id = "";
+        navButtonBarMid.id = "";
+        navButtonBarBot.id = "";
+    } else {
+        navButtonBarTop.id = "nav-button-top";
+        navButtonBarMid.id = "nav-button-middle";
+        navButtonBarBot.id = "nav-button-bottom";
+    }
+
+    navOpen = !navOpen;
 }
 
-var sections = ["banner", "contact", "work", "life", "misc"];
+// var lastActiveSectionElement;
 
-document.addEventListener("scroll", () => {
-    let closest = 30000;
-    let visibleElement;
-    for (let i = 0; i < sections.length; i++) {
-        let current = document.getElementById(sections[i]);
-        let top = current.getBoundingClientRect().top;
+// // scroll to element with id of destination
+// function navTo(destination) {
+//     var elmnt = document.getElementById(destination);
+//     elmnt.scrollIntoView({ behavior: "smooth" });
+// }
 
-        if (Math.abs(top) < closest) {
-            closest = Math.abs(top);
-            visibleElement = current;
-        }
-    }
+// var sections = ["banner", "contact", "work", "life", "misc"];
 
-    if (visibleElement.id === "banner") {
-        if (lastActiveSectionElement) {
-            lastActiveSectionElement.classList.remove("active-section");
-        }
-        return;
-    }
+// document.addEventListener("scroll", () => {
+//     let closest = 30000;
+//     let visibleElement;
+//     for (let i = 0; i < sections.length; i++) {
+//         let current = document.getElementById(sections[i]);
+//         let top = current.getBoundingClientRect().top;
 
-    if (visibleElement) {
-        // console.log(visibleElement.id);
+//         if (Math.abs(top) < closest) {
+//             closest = Math.abs(top);
+//             visibleElement = current;
+//         }
+//     }
 
-        if (lastActiveSectionElement) {
-            lastActiveSectionElement.classList.remove("active-section");
-        }
+//     if (visibleElement.id === "banner") {
+//         if (lastActiveSectionElement) {
+//             lastActiveSectionElement.classList.remove("active-section");
+//         }
+//         return;
+//     }
 
-        var current = document.getElementById(
-            visibleElement.id + "-nav-button"
-        );
-        current.classList.add("active-section");
-        lastActiveSectionElement = current;
-    }
-});
+//     if (visibleElement) {
+//         // console.log(visibleElement.id);
+
+//         if (lastActiveSectionElement) {
+//             lastActiveSectionElement.classList.remove("active-section");
+//         }
+
+//         var current = document.getElementById(
+//             visibleElement.id + "-nav-button"
+//         );
+//         current.classList.add("active-section");
+//         lastActiveSectionElement = current;
+//     }
+// });
