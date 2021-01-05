ed07e5f05bc88ce3c12fbf048e78a1157eaa9764
mljustinli <ml.justin.li@gmail.com>
2021-01-05 15:22:51 -0500

    Make nav button open up list of nav sections with transitions

--- js/nav.js
+++ js/nav.js
@@ -3,18 +3,22 @@ let navButton = document.getElementById("nav-button");
 let navButtonBarTop = navButton.children[0];
 let navButtonBarMid = navButton.children[1];
 let navButtonBarBot = navButton.children[2];
+let navSections = document.getElementById("nav-sections");
+let navDarken = document.getElementById("nav-darken");
 
 function navButtonClicked() {
-    console.log(navButton);
-    console.log(navButtonBarTop.id);
     if (navOpen) {
         navButtonBarTop.id = "";
         navButtonBarMid.id = "";
         navButtonBarBot.id = "";
+        navSections.classList.remove("nav-sections-open");
+        navDarken.classList.remove("nav-darken-open");
     } else {
         navButtonBarTop.id = "nav-button-top";
         navButtonBarMid.id = "nav-button-middle";
         navButtonBarBot.id = "nav-button-bottom";
+        navSections.classList.add("nav-sections-open");
+        navDarken.classList.add("nav-darken-open");
     }
 
     navOpen = !navOpen;
@@ -22,11 +26,12 @@ function navButtonClicked() {
 
 // var lastActiveSectionElement;
 
-// // scroll to element with id of destination
-// function navTo(destination) {
-//     var elmnt = document.getElementById(destination);
-//     elmnt.scrollIntoView({ behavior: "smooth" });
-// }
+// scroll to element with id of destination
+function navTo(destination) {
+    navButtonClicked();
+    var elmnt = document.getElementById(destination);
+    elmnt.scrollIntoView({ behavior: "smooth" });
+}
 
 // var sections = ["banner", "contact", "work", "life", "misc"];
 
