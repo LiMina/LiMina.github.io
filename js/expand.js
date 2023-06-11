ff39331ff12851edcd9b2a2720718bb9d89fa165
Mina Li <li.mina2007@gmail.com>
2023-06-11 11:46:26 -0700

    wip

--- js/expand.js
+++ js/expand.js
@@ -1,20 +1,30 @@
 var workHeaderElements = document.getElementsByClassName("work-header");
 var projectHeaderElements = document.getElementsByClassName("project-header");
 var descriptionElements = document.getElementsByClassName("description");
+var chevronElements = document.getElementsByClassName("chevron");
 
 var headerFunction = function () {
+    console.log(descriptionElements);
+    console.log(chevronElements);
     let description = this.parentElement.children[
         this.parentElement.children.length - 1
     ];
+    let chevron = this.parentElement.children[0].children[1];
+    console.log(chevron);
 
     let previouslyOpen = description.classList.contains("description-open");
+    let previouslyFlipped = chevron.classList.contains("chevron-flip");
 
     for (let i = 0; i < descriptionElements.length; i++) {
         descriptionElements[i].classList.remove("description-open");
+        if (i < 6) {
+            chevronElements[i].classList.remove("chevron-flip");
+        }
     }
 
     if (!previouslyOpen) {
         description.classList.add("description-open");
+        chevron.classList.add("chevron-flip")
     }
 };
 
